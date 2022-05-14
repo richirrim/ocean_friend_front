2303c0b09a823ef883b477f7615d6d17b1e61c31
richirrim <richirrim@DESKTOP-B3JUEMT.localdomain>
2022-05-14 18:07:16 -0500

    Añade las fuentes de Google Font al proyecto.

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,9 @@
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Koulen&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Varela+Round&display=swap');
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
