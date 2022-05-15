1cf3d751ad8d48192d156a7bbbe5d156aa89aa18
Andrea <andrea.rueda-ext@dominion-global.com>
2022-05-15 15:31:43 -0500

    intentando agregar google maps

---
+++ src/components/GoogleMapC.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <GoogleMap
+      api-key="AIzaSyCWrEX8F6my1xYIFzbG2i2b_CkmHro6g8U"
+      style="width: 100%"
+      :center="center"
+      :zoom="15"
+    >
+      <Marker :options="{ position: center }" />
+    </GoogleMap>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { GoogleMap, Marker } from 'vue3-google-map'
+
+export default defineComponent({
+  name: 'GoogleMaping',
+  components: { GoogleMap, Marker },
+  setup () {
+    const center = { lat: 40.689247, lng: -74.044502 }
+
+    return { center }
+  }
+})
+</script>
